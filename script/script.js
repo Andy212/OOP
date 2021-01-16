b42b7c73b498420509236977535e165ab5dd63eb
Andy Demichev <Andyuson24@gmail.com>
2021-01-16 20:38:36 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -13,26 +13,29 @@ const DomElement = function(selector, height, width, bg, fontSize){
 
 DomElement.prototype.creator = function(){
 
-    switch(this.selector){
-        case '.block':
-            const newElem = document.createElement('div');
+    
+
+
+    if(this.selector.substring(0,1) === '.'){
+        const newElem = document.createElement('div');
             newElem.classList.add('block');
             elems.append(newElem);
             newElem.textContent = 'Блок DIV';
     
             newElem.style.cssText =`width: ${this.width}; height: ${this.height}; background: ${this.bg}; font-size: ${this.fontSize};`;
-            break;
-            case '#best':
-                const newElem2 = document.createElement('p');
-                elems.append(newElem2);    
-                newElem2.textContent = 'Блок P';
-    
-                newElem2.style.cssText =`width: ${this.width}; height: ${this.height}; background: ${this.bg}; font-size: ${this.fontSize};`;
-                break;
+    } else if(this.selector.substring(0,1) === '#'){
+        const newElem2 = document.createElement('p');
+        elems.append(newElem2);    
+        newElem2.textContent = 'Блок P';
+
+        newElem2.style.cssText =`width: ${this.width}; height: ${this.height}; background: ${this.bg}; font-size: ${this.fontSize};`;
+    }else{
+        console.log('Неверный символ')
     }
+
 
 };
 
-let newElement = new DomElement ('.block', '100px', '100px', 'red', '20px');
+let newElement = new DomElement ('.', '100px', '100px', 'red', '20px');
 newElement.creator();
 
